0b1fce076c8f5f947bf95733978507530e0640f6
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-04-06 21:30:27 +0400

    add state slices

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import './store'
+import store from './store'
+
+//example
+store.dispatch({
+  type: 'account/deposit',
+  payload: 1000
+})
+console.log(store.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
