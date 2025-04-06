7eea1309837d8a2a6f1e361a33bb909cc5447690
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-04-06 20:47:58 +0400

    add customer stote

--- src/store.js
+++ src/store.js
@@ -1,12 +1,18 @@
-import { createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: ''
 }
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: '',
+  nationalID: '',
+  createdAt: ''
+}
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case 'account/deposit':
       return {
@@ -38,7 +44,31 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer)
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case 'customer/create':
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt
+      }
+    case 'customer/updateName':
+      return {
+        ...state,
+        fullName: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer
+})
+
+const store = createStore(rootReducer)
 
 // store.dispatch({ type: 'account/deposit', payload: 1000 })
 // console.log(store.getState()) // { balance: 1000, loan: 0, loanPurpose: '' };
@@ -82,4 +112,20 @@ store.dispatch(withdraw(200))
 store.dispatch(requestLoan(500, 'car'))
 store.dispatch(payLoan())
 
-export { store, deposit, withdraw, requestLoan, payLoan }
+function createCustomer(fullName, nationalID) {
+  return {
+    type: 'customer/create',
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() }
+  }
+}
+
+function updateName(fullName) {
+  return {
+    type: 'customer/updateName',
+    payload: fullName
+  }
+}
+
+store.dispatch(createCustomer('John Doe', '123456789'))
+store.dispatch(updateName('Jane Doe'))
+console.log(store.getState()) // { account: { balance: 300, loan: 0, loanPurpose: '' }, customer: { fullName: 'Jane Doe', nationalID: '123456789', createdAt: '2023-10-03T12:34:56.789Z' } }
