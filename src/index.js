80890cb1d97a437007d4ff8a049c59dbacebd3cf
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-04-06 21:37:36 +0400

    connect redux to react app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+
 import './index.css'
 import App from './App'
-import store from './store'
 
-//example
-store.dispatch({
-  type: 'account/deposit',
-  payload: 1000
-})
-console.log(store.getState())
+import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
