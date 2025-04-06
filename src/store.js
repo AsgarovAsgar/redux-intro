86526ce1be64ef687d1e378bbbbd47e53ecf861a
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-04-06 20:33:44 +0400

    add action creators

--- src/store.js
+++ src/store.js
@@ -40,14 +40,46 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer)
 
-store.dispatch({ type: 'account/deposit', payload: 1000 })
-console.log(store.getState()) // { balance: 1000, loan: 0, loanPurpose: '' };
+// store.dispatch({ type: 'account/deposit', payload: 1000 })
+// console.log(store.getState()) // { balance: 1000, loan: 0, loanPurpose: '' };
 
-store.dispatch({ type: 'account/withdraw', payload: 200 })
-console.log(store.getState()) // { balance: 800, loan: 0, loanPurpose: '' };
+// store.dispatch({ type: 'account/withdraw', payload: 200 })
+// console.log(store.getState()) // { balance: 800, loan: 0, loanPurpose: '' };
 
-store.dispatch({ type: 'account/requestLoan', payload: { amount: 500, purpose: 'car' } })
-console.log(store.getState()) // { balance: 800, loan: 500, loanPurpose: 'car' };
+// store.dispatch({ type: 'account/requestLoan', payload: { amount: 500, purpose: 'car' } })
+// console.log(store.getState()) // { balance: 800, loan: 500, loanPurpose: 'car' };
 
-store.dispatch({ type: 'account/payLoan' })
-console.log(store.getState()) // { balance: 300, loan: 0, loanPurpose: '' };
+// store.dispatch({ type: 'account/payLoan' })
+// console.log(store.getState()) // { balance: 300, loan: 0, loanPurpose: '' };
+
+function deposit(amount) {
+  return {
+    type: 'account/deposit',
+    payload: amount
+  }
+}
+function withdraw(amount) {
+  return {
+    type: 'account/withdraw',
+    payload: amount
+  }
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: { amount, purpose }
+  }
+}
+function payLoan() {
+  return {
+    type: 'account/payLoan'
+  }
+}
+
+// Example usage
+store.dispatch(deposit(1000))
+store.dispatch(withdraw(200))
+store.dispatch(requestLoan(500, 'car'))
+store.dispatch(payLoan())
+
+export { store, deposit, withdraw, requestLoan, payLoan }
